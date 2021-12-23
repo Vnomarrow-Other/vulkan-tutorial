a78210d93e8b98fb1c0a5154a9d137a09f2356e4
Vnomarrow <victor.millberg@gmail.com>
2021-12-23 02:52:22 +0100

    load multiple models, change model count using arrow keys

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 The project uses code from a tutorial, but the code has been modified
 
 link:
-https://kylemayes.github.io/vulkanalia/model/loading_models.html
+https://kylemayes.github.io/vulkanalia/dynamic/secondary_command_buffers.html
 
 tutorial licence:
 SPDX-License-Identifier: Apache-2.0
---
+++ src/main copy.rs
@@ -0,0 +1,60 @@
+/*#![allow(
+    dead_code,
+    unused_variables,
+    clippy::too_many_arguments,
+    clippy::unnecessary_wraps
+)]
+
+pub mod app;
+pub mod device_selection;
+pub mod pipeline;
+pub mod setup;
+pub mod use_decl;
+
+use app::*;
+use use_decl::*;
+
+#[rustfmt::skip]
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    // Window
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Vulkan Tutorial (Rust)")
+        .with_inner_size(LogicalSize::new(1024, 768))
+        .build(&event_loop)?;
+
+    // App
+
+    let mut app = unsafe { App::create(&window)? };
+    let mut destroying = false;
+    let mut minimized = false;
+
+    // Run the app, also add event handling
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            // Render a frame if our Vulkan app is not being destroyed.
+            Event::MainEventsCleared if !destroying && !minimized => unsafe { app.render(&window) }.unwrap(),
+            // Mark the window as having been resized.
+            Event::WindowEvent { event: WindowEvent::Resized(size), .. } => {
+                if size.width == 0 || size.height == 0 {
+                    minimized = true;
+                } else {
+                    minimized = false;
+                    app.resized = true;
+                }
+            }
+            // Destroy our Vulkan app.
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                destroying = true;
+                *control_flow = ControlFlow::Exit;
+                unsafe { app.destroy(); }
+            }
+            _ => {}
+        }
+    });
+}
+*/
